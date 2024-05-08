9f9c35662340374c12ab621092e54ecf7ba56cf3
Powerfuljackell <johnsonjakel@aol.com>
2024-05-08 10:03:54 -0700

    Updated code.js

--- code.js
+++ code.js
@@ -10,10 +10,11 @@ function factorial(n) {
 
 function e(n) {
     var final = 1;
+    var factTemp = 1;
     if(n === 0) return final;
     for(let i = 1; i <=n; i++){
-        final += 1/factorial(i)
+        final += 1/(factTemp*=i);
     }
    return final;
 }
-//console.log(e(1000));
+//console.log(e(10000));
